5d74500b09d76641dca3ed74eff6e68f2804e9c3
Сергей Букарин <SerBuka2006@yandex.ru>
2024-04-22 16:20:36 +0300

    AdS 5

--- include/tstack.h
+++ include/tstack.h
@@ -2,9 +2,42 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+#include <string>
+
+template<typename T, int Size>
 class TStack {
-  // добавьте код стека
+private:
+    T data[Size];
+    int t;
+
+public:
+    TStack() : t(0) {}
+
+    T pop() {
+        if (t > 0)
+            return data[--t];
+        else
+            throw std::string("empty");
+    }
+
+    void push(const T& item) {
+        if (t <= Size - 1)
+            data[t++] = item;
+        else
+            throw std::string("full");
+    }
+
+
+    bool isEmpty() {
+        return t == 0;
+    }
+
+    T get() {
+        if (!isEmpty())
+            return data[t - 1];
+        else
+            throw std::string("empty");
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,123 @@
 #include <map>
 #include "tstack.h"
 
+TStack<char, 100> stackf;
+TStack<int, 100> stacks;
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+    std::string out;
+    for (char indicator : inf) {
+        if (indicator == 40) {
+            stackf.push(indicator);
+        } else if (indicator >= 48 && indicator <= 57) {
+            out += indicator;
+            out += 32;
+        } else if (indicator == 41) {
+            while (stackf.get() != 40 && !stackf.isEmpty()) {
+                out += stackf.pop();
+                out += 32;
+            }
+            if (stackf.get() == 40)
+                stackf.pop();
+        } else if (indicator == 43 || indicator == 45) {
+            if (!stackf.isEmpty()) {
+                switch (stackf.get()) {
+                case 43: {
+                    out += 43;
+                    out += 32;
+                    stackf.pop();
+                    break;
+                }
+                case 45: {
+                    out += 45;
+                    out += 32;
+                    stackf.pop();
+                    break;
+                }
+                case 42: {
+                    out += 42;
+                    out += 32;
+                    stackf.pop();
+                    break;
+                }
+                case 47: {
+                    out += 47;
+                    out += 32;
+                    stackf.pop();
+                    break;
+                }
+                }
+                stackf.push(indicator);
+            } else {
+                stackf.push(indicator);
+            }
+        } else if (indicator == 42 || indicator == 47) {
+            if (!stackf.isEmpty()) {
+                switch (stackf.get()) {
+                case 42: {
+                    out += 42;
+                    out += 32;
+                    stackf.pop();
+                    break;
+                }
+                case 47: {
+                    out += 47;
+                    out += 32;
+                    stackf.pop();
+                    break;
+                }
+                }
+                stackf.push(indicator);
+            } else {
+                stackf.push(indicator);
+            }
+        }
+    }
+    if (!stackf.isEmpty()) {
+        while (!stackf.isEmpty()) {
+            out += stackf.pop();
+            out += 32;
+        }
+        out.pop_back();
+    }
+    return out;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+    std::string str;
+    char S;
+    for (char indicator : pref) {
+        if ((indicator >= 48 && indicator <= 57)) {
+            str += indicator;
+
+        } else if (indicator == 32 && !str.empty()) {
+            stacks.push(std::stoi(str));
+            str.clear();
+        } else if (indicator == 43 || indicator == 45 ||
+            indicator == 42 || indicator == 47) {
+            switch (indicator) {
+            case 43: {
+                S = stacks.pop();
+                stacks.push(stacks.pop() + S);
+                break;
+            }
+            case 45: {
+                S = stacks.pop();
+                stacks.push(stacks.pop() - S);
+                break;
+            }
+            case 42: {
+                S = stacks.pop();
+                stacks.push(stacks.pop() * S);
+                break;
+            }
+            case 47: {
+                S = stacks.pop();
+                stacks.push(stacks.pop() / S);
+                break;
+            }
+            }
+        }
+    }
+    return stacks.pop();
 }
